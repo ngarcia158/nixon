94db6b6983d738d0a6193e3711de102696eaf551
NIcholas <Nicholasgarcia947@gmail.com>
2021-04-20 15:22:19 -0700

    Started adding a mouse over that makes text

--- pictures.js
+++ pictures.js
@@ -1,27 +1,27 @@
 
-window.addEventListener('contextmenu', e => {
-    e.preventDefault();
-  });
+// window.addEventListener('contextmenu', e => {
+// //     e.preventDefault();
+// //   });
   
-function noAlert(){
-    this.alert('Please Choose the correct candidate');
-}
-function yesAlert(){
-    this.alert('Thank you for your vote')
-}
-function perhapsAlert(){
-    this.alert('VOTE NIXON');
-}
-window.onload = setTimeout(() => {
-        this.alert('Make the right decision america is watching');   
-}, 100);
-var richard = 0;
-var nixon = 0;
-var is = 0;
-var the = 0;
-var best = 0;
+// function noAlert(){
+//     this.alert('Please Choose the correct candidate');
+// }
+// function yesAlert(){
+//     this.alert('Thank you for your vote')
+// }
+// function perhapsAlert(){
+//     this.alert('VOTE NIXON');
+// }
+// window.onload = setTimeout(() => {
+//         this.alert('Make the right decision america is watching');   
+// }, 100);
+var richard = `<div class="image"><img class="img" src="assets/nixon4.jpeg" alt="pablo"> <div class="overlay"><div class="imageTitle">Vote</div><p class="imageDescription" </p></div></div>`
+var nixon = `<div class="image"><img class="img" src="assets/nixon5.jpeg" alt="pablo"><div class="overlay"><div class="imageTitle"><p id="noContextMenu">Richard</p></div><p class="imageDescription"></p></div></div>`;
+var is = `<div class="image"><img class="img" src="assets/nixon6.jpeg" alt="pablo"><div class="overlay"><div class="imageTitle noContextMenu">Nixon</div>    <p class="imageDescription"></p></div></div></div>`;
+var the = `<div class="image"><img class="img" src="assets/nixon7.jpeg" alt="pablo"><div class="overlay"><div class="imageTitle noContextMenu">For</div><p class="imageDescription"></p></div></div>`;
+var best = `div class="image"><img class="img" src="assets/nixon3.jpeg" alt="pablo"><div class="overlay"><div class="imageTitle noContextMenu">President</div><p class="imageDescription"></p></div></div>`;
 
-
+var displayArray = [richard, nixon, is, the, best];
 // var arrayForDivImages = [richard, nixon, is, the, best]
 // var div = document.getElementById('cluster')
 // function leaveFunction(){
@@ -29,4 +29,28 @@ var best = 0;
 //        div += arrayForDivImages[i]
 //     }
 // };
+document.body.onload = placeItems();
 
+
+function placeItems () {
+//   // create a new div element
+//   const newDiv = document.getElementById("cluster").createElement("div");
+
+//   // and give it some content
+//   const newContent = document.createTextNode("Hi there and greetings!");
+
+//   // add the text node to the newly created div
+//   newDiv.appendChild(newContent);
+
+//   // add the newly created element and its content into the DOM
+//   const currentDiv = document.getElementById("div1");
+//   document.body.after();
+    // var node = document.createElement("div");
+    // var textnode = document.createTextNode("Water");
+    // node.appendChild(textnode);
+    // document.getElementById("cluster").appendChild(node);
+    // node.setAttribute('id', 'image')
+
+    
+
+}
